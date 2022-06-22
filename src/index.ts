68b2ffc851ae314a5a588be3f380ef282f1f9ee0
Lord-Valen <lord_valen@protonmail.com>
2022-06-22 15:03:32 -0400

    Dump

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { HadesContainer } from 'hades'
 import 'reflect-metadata'
+
+import { HadesContainer } from 'hades'
 import { BotService } from './services/BotService';
 
 const container = new HadesContainer();
+
 const bot = container.get(BotService);
 bot.login()
